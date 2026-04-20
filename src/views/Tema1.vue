a4ec309350f8053409a85f32f484cc875a23f8b5
Henry Alvarez <halvarezast@gmail.com>
2026-04-20 09:19:32 -0500

    Update Videos and Podcast

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -442,7 +442,7 @@
     //- Inicio Video
     figure.mb-5(data-aos="fade-down")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/nHjl83QvSUw?si=PYOSxqLdAk88V5nb" title="Conductas naturales en sistemas semicriollos." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/qEc6WgCeA78?si=GNTq_tBBxQNW5oep" title="Conductas naturales en sistemas semicriollos." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     //- Fin Video
     //- Título con imagen
     .bg-full-width.mt-5
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -436,7 +436,7 @@
     //- Inicio Video
     figure.mb-5(data-aos="fade-down")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/nHjl83QvSUw?si=PYOSxqLdAk88V5nb" title="Conductas naturales en sistemas semicriollos." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/85ZZb8qxtjk?si=YlsHV4HkRTyNStyE" title="Conductas naturales en sistemas semicriollos." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     //- Fin Video
     Separador
     //- Inicio Tema2.3
